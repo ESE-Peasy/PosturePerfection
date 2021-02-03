19c87b8d5ba41f31f79aeda9e1d572e39c8282af
mhr <2247469r@student.gla.ac.uk>
2021-02-03 17:12:14 +0000

    Remove previously generated documentation
    To test if new docs are created

--- docs/html/hello_8h.js
+++
@@ -1,4 +0,0 @@
-var hello_8h =
-[
-    [ "hello_world", "hello_8h.html#a8674ffb3345a47f83692fb3988e4037b", null ]
-];
--- docs/html/search/files_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['hello_2ecpp_8',['hello.cpp',['../hello_8cpp.html',1,'']]],
-  ['hello_2eh_9',['hello.h',['../hello_8h.html',1,'']]]
-];
