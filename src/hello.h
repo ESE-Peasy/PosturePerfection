4a5fecbfcf9c8644d1e3ab0fd25ba6139814a083
mhr <2247469r@student.gla.ac.uk>
2021-02-03 09:55:26 +0000

    Remove trailing white space

--- src/hello.h
+++ src/hello.h
@@ -1,7 +1,7 @@
 /**
  * @file hello.h
  * @brief A hello world function
- * 
+ *
  */
 
 #ifndef SRC_HELLO_H_
@@ -9,7 +9,7 @@
 
 /**
  * @brief Example to print "Hello, world!"
- * 
+ *
  */
 void hello_world(void);
 #endif  // SRC_HELLO_H_
