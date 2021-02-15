ca4212cbbe04e091dabfa9f637238d7fd0d1f169
mhr <2247469r@student.gla.ac.uk>
2021-02-15 18:39:05 +0000

    Add documentation for inference_core.h

--- src/inference_core.h
+++ src/inference_core.h
@@ -1,3 +1,9 @@
+/**
+ * @file inference_core.h
+ * @brief Wrapper API for the TensorFlow Lite model
+ *
+ */
+
 #ifndef INFERENCE_CORE_H
 #define INFERENCE_CORE_H
 
@@ -35,6 +41,16 @@ typedef struct PreprocessedImage {
   uint8_t channels = 3;
 } PreprocessedImage;
 
+/**
+ * @brief A wrapper to make running inference easier
+ *
+ * This class hides some of the complexity of the underlying TensorFlow Lite C++
+ * API. It nonetheless expects preprocessed images. This means images will need
+ * to be resized to the model input dimensions and normalised from `uint8_t`
+ * values to `float`s in the interval [-1..1] before being run through a
+ * `InferenceCore` object.
+ *
+ */
 class InferenceCore {
  private:
   std::unique_ptr<tflite::Interpreter> interpreter;
@@ -43,12 +59,27 @@ class InferenceCore {
   size_t model_input_size;
   uint8_t model_input_channels = 3;
 
-
   Coordinate pixel_coord_to_Coordinate(uint32_t x, uint32_t y);
 
  public:
+  /**
+   * @brief Construct a new Inference Core object
+   *
+   * @param model_filename Path to the TensorFlow Lite model
+   * @param model_input_width Width of the input to the model
+   * @param model_input_height Height of the input to the model
+   */
   InferenceCore(const char* model_filename, size_t model_input_width,
                 size_t model_input_height);
+
+  /**
+   * @brief Run a pre-processed image through the loaded model
+   *
+   * @param preprocessed_image An image that has been resized to the model input
+   * dimensions and normalised from `uint8_t` values to `float`s in the interval
+   * [-1..1] before being passed to this function
+   * @return InferenceResults
+   */
   InferenceResults run(PreprocessedImage preprocessed_image);
 };
 
