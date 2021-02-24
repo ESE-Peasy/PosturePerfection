a023aa4b4547c78c36ed8bb528ce17f859f33164
mhr <2247469r@student.gla.ac.uk>
2021-02-24 14:04:38 +0000

    Add new test script
    To be run before merging to main

---
+++ test.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+echo "Starting ..."
+
+## Check for cpplint
+if ! cpplint --version >/dev/null 2>&1
+then
+    echo "cpplint is not installed; please install with:"
+    echo "    pip3 install cpplint"
+    exit 1
+fi
+
+printf "Running cpplint ... \r"
+cpplint --filter=-build/include_subdir --recursive --quiet ./src/*
+echo "Running cpplint ... DONE"
+
+echo "Starting build ... "
+
+./install.sh || { echo "Problem building, see above for specific errors"; exit 1; }
+
+echo "Running ctest ... "
+
+cd build
+ctest || { cd ..; exit 1; }
+
+cd ..
