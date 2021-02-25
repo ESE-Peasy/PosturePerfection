3774b0a4b940e65e0d6118e5d8ef4cc4d9b31f77
Andrew-Ritchie <2253409r@student.gla.ac.uk>
2021-02-25 17:23:38 +0000

    added copyright statements and example push button

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -1,9 +1,65 @@
+/**
+ * @file mainwindow.cpp
+ * @brief Main user interface file
+ *
+ * @copyright Copyright (C) 2021  Andrew Ritchie
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 #include "mainwindow.h"
 
 
 MainWindow::MainWindow(QWidget *parent) :
-  QMainWindow(parent)
+  QMainWindow(parent) {
+  //centralWidget = new QWidget();
+
+
+  // Create the button, make "this" the parent
+  m_button = new QPushButton("My Button", this);
+  // set size and location of the button
+  m_button->setGeometry(QRect(QPoint(100, 100), QSize(200, 50)));
+ 
+  // Connect button signal to appropriate slot
+  connect(m_button, &QPushButton::released, this, &MainWindow::handleButton);
+
+
+
+
+  /*
+	frame = new QFrame(centralWidget);
+	frame->setFrameShape(QFrame::StyledPanel);
+	frame->setFrameShadow(QFrame::Raised);
+
+	verticalLayout_2 = new QVBoxLayout(frame);
+	verticalLayout_2->setSpacing(6);
+	verticalLayout_2->setContentsMargins(11, 11, 11, 11);
+
+	label = new QLabel(frame);
+	verticalLayout_2->addWidget(label);
+	verticalLayout->addWidget(frame);
+	setCentralWidget(centralWidget);
+	statusBar = new QStatusBar();
+	setStatusBar(statusBar);
+  */
+    
+}
+
+void MainWindow::handleButton()
 {
-
-
+  // change the text
+  m_button->setText("Example");
+  // resize button
+  m_button->resize(100,100);
 }
--- src/gui/mainwindow.h
+++ src/gui/mainwindow.h
@@ -1,5 +1,25 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+/**
+ * @file mainwindow.h
+ * @brief Header file for the main window
+ *
+ * @copyright Copyright (C) 2021  Andrew Ritchie
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef SRC_GUI_MAINWINDOW_H_
+#define SRC_GUI_MAINWINDOW_H_
 
 #include <QMainWindow>
 #include <QInputDialog>
@@ -13,50 +33,43 @@
 #include <QStatusBar>
 #include <QVBoxLayout>
 #include <QWidget>
+#include <QPushButton>
+#include <QRect>
 
 
 
-class MainWindow : public QMainWindow
-{
-	Q_OBJECT
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow{
+  Q_OBJECT
+
+ public:
+  explicit MainWindow(QWidget *parent = 0);
+
+
+ private:
+  /*
+  QWidget *centralWidget;
+  QVBoxLayout *verticalLayout;
+  QFrame *frame_2;
+  QVBoxLayout *verticalLayout_3;
+  QCustomPlot *customPlot;
+  QFrame *frame;
+  QVBoxLayout *verticalLayout_2;
+  QLabel *label;
+  QMenuBar *menuBar;
+  QStatusBar *statusBar;
+  */   
+  QPushButton *m_button;
+
+ private slots:
+  void handleButton();
+
   
-public:
-	explicit MainWindow(QWidget *parent = 0);
-/*
-  	void timerEvent( QTimerEvent * );
-	void addRealtimeSample(double v);
 
-private slots:
-	void titleDoubleClick(QMouseEvent *event);
-	void axisLabelDoubleClick(QCPAxis* axis, QCPAxis::SelectablePart part);
-	void legendDoubleClick(QCPLegend* legend, QCPAbstractLegendItem* item);
-	void selectionChanged();
-	void mousePress();
-	void mouseWheel();
-	void addRandomGraph();
-	void addRealtimeGraph();
-	void removeSelectedGraph();
-	void removeAllGraphs();
-	void contextMenuRequest(QPoint pos);
-	void moveLegend();
-	void graphClicked(QCPAbstractPlottable *plottable, int dataIndex);
-  
-private:
-	QWidget *centralWidget;
-	QVBoxLayout *verticalLayout;
-	QFrame *frame_2;
-	QVBoxLayout *verticalLayout_3;
-	QCustomPlot *customPlot;
-	QFrame *frame;
-	QVBoxLayout *verticalLayout_2;
-	QLabel *label;
-	QMenuBar *menuBar;
-	QStatusBar *statusBar;
-	QSharedPointer<QCPDataContainer<QCPGraphData> > animdata;
-	const int nRealtimePoints = 500;
-	const double dt = 0.02;
-	double t = 0;
-*/
 };
 
-#endif // MAINWINDOW_H
+#endif  // SRC_GUI_MAINWINDOW_H_
