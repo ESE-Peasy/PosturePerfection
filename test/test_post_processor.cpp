29819fb3a3296c6b8c0413d9e19f2271453c5596
mhr <2247469r@student.gla.ac.uk>
2021-02-25 13:11:47 +0000

    Update to make testing post processor possible

--- test/test_post_processor.cpp
+++ test/test_post_processor.cpp
@@ -1,9 +1,11 @@
 #include <boost/test/unit_test.hpp>
 
-#include "post_processor.h"
+#include "../src/iir.h"
+#include "../src/post_processor.h"
 
 BOOST_AUTO_TEST_CASE(PassTest) {
-  float** coeffs = {{0, 0, 0, 0, 0, 0}};
-  settings = PostProcessing::SmoothingSettings{coeffs, 1};
-  post_proc = PostProcessing::PostProcessor(1.0, settings);
+  float** coeffs = nullptr;
+  IIR::SmoothingSettings settings = IIR::SmoothingSettings{coeffs, 0};
+  PostProcessing::PostProcessor post_proc =
+      PostProcessing::PostProcessor(1.0, settings);
 }
