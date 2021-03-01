f3e73d3f782599df5c5a03f6011c691a1e723136
Ashwin-MJ <ashwinmj@gmail.com>
2021-03-01 14:05:18 +0000

    Fix dependencies script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -50,19 +50,6 @@ cd opencv_build
 cmake -DCMAKE_BUILD_TYPE=Release ../opencv_src || exit 1
 make || exit 1
 
-## Download OpenCV code
-cd ..
-echo "Downloading OpenCV ..."
-if [ ! -d "opencv_src" ]; then
-  git clone https://github.com/opencv/opencv.git opencv_src
-fi
-
-## Build OpenCV
-mkdir -p opencv_build
-cd opencv_build
-cmake -DCMAKE_BUILD_TYPE=Release ../opencv_src
-make -j7
-
 ## Reset directory to project root
 cd $PP_ROOT
 
