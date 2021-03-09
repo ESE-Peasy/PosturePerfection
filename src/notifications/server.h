1686d40b81e037fc3db0d3a3b7463164a6d306d1
Conor Begley <conorbegley@outlook.com>
2021-03-09 10:24:25 +0000

    server header file

---
+++ src/notifications/server.h
@@ -0,0 +1,82 @@
+/**
+ * @file notify_server.h
+ * @brief Local server for receiving notifications from the Raspberry Pi. Only
+ * runs on Linux
+ *
+ * @copyright Copyright (C) 2021  Conor Begley
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SRC_NOTIFICATIONS_SERVER_H_
+#define SRC_NOTIFICATIONS_SERVER_H_
+
+#include <stdio.h>
+
+#include <string>
+
+namespace Notify {
+/**
+ * @brief A server which receives notifications about Posture from the Raspberry
+ * Pi and updates the user accordingly
+ *
+ */
+class NotifyServer {
+ private:
+  /**
+   * @brief Sends a posture message using notify-send
+   */
+  void sendDesktopNotification();
+  /**
+   * @brief Checks for incoming message
+   */
+  bool checkIncoming();
+
+  /**
+   * @brief Returns string of message from client to server
+   */
+  std::string getMessage();
+
+  /**
+   * @brief General purpose socket for creating other sockets
+   */
+  int server_fd;
+
+  /**
+   * @brief Specific socket to connect to Pi and listen for incoming
+   * messages
+   */
+  int client_fd;
+
+ public:
+  /**
+   * @brief Constructor for notify server
+   *
+   * @param port port number to listen on (default is 8080)
+   * @param string hostname (default is raspberrypi.local)
+   * @param ip address of raspberry pi (if set to any value,
+   * hostname string is ignored)
+   */
+  NotifyServer(std::string hostname = "raspberrypi.local", int port = 8080,
+               char* ip = nullptr);
+  ~NotifyServer();
+
+  /**
+   * @brief Sets the server to start listening
+   */
+  int run();
+};
+}  // namespace Notify
+#endif  // SRC_NOTIFICATIONS_SERVER_H_
