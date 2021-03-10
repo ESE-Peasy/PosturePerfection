552e5541c8c783c6d63afd3fcb63e8f97bc24e35
Conor Begley <conorbegley@outlook.com>
2021-03-10 13:36:43 +0000

    Tidied pointers

--- src/notifications/server.cpp
+++ src/notifications/server.cpp
@@ -3,30 +3,31 @@
 namespace Notify {
 
 NotifyServer::NotifyServer(int port) {
-  server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(port);
+  this->server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  this->address.sin_family = AF_INET;
+  this->address.sin_addr.s_addr = INADDR_ANY;
+  this->address.sin_port = htons(port);
   int opt = 1;
-  int setup = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                         &opt, sizeof(opt));
+  int setup = setsockopt(this->server_fd, SOL_SOCKET,
+                         SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 
   int addrlen = sizeof(address);
-  int binding = bind(server_fd, (struct sockaddr *)&address, addrlen);
+  int binding =
+      bind(this->server_fd, (struct sockaddr *)&(this->address), addrlen);
   Notify::err_msg(binding, "socket_bind");
-  int listening = listen(server_fd, 5);
+  int listening = listen(this->server_fd, 5);
   Notify::err_msg(binding, "socket_listen");
 }
 NotifyServer::~NotifyServer() {
-  shutdown(server_fd, SHUT_RDWR);
-  close(server_fd);
+  shutdown(this->server_fd, SHUT_RDWR);
+  close(this->server_fd);
 }
 
 void NotifyServer::run() {
   std::memset(buffer, 0, sizeof(buffer));
 
-  int pi_fd =
-      accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+  int pi_fd = accept(this->server_fd, (struct sockaddr *)&(this->address),
+                     (socklen_t *)&(this->addrlen));
 
   int read_value = read(pi_fd, buffer, 1024);
   Notify::err_msg(read_value, "socket_reading");
