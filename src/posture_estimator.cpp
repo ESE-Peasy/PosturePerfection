928870cf39157e0621c8c3c54c0a354270cb5355
Conor Begley <conorbegley@outlook.com>
2021-03-15 11:19:22 +0000

    Further spacing fixes in Posture Estimator

--- src/posture_estimator.cpp
+++ src/posture_estimator.cpp
@@ -2,7 +2,7 @@
  * @file posture_estimator.h
  * @brief Interface for representation of user's pose
  *
- * @copyright Copyright (C) 2021 Conor Begley
+ * @copyright Copyright (C) 2021  Conor Begley
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -11,11 +11,11 @@
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
 
