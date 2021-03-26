be7a8b2267c6e1fe4345afec6e6bfac22ae66133
Ashwin <ashwinmj@gmail.com>
2021-03-26 14:34:01 +0000

    Update install script

--- PosturePerfection_install.sh
+++ PosturePerfection_install.sh
@@ -20,14 +20,14 @@ case $OS  in
     *"armv7"*)
         wget "https://github.com/ESE-Peasy/opencv/releases/download/v4.5.1-pp/opencv-4.5.1_armhf.deb"
         sudo apt install ./opencv-4.5.1_armhf.deb
-        wget "https://github.com/ESE-Peasy/PosturePerfection/releases/download/v0.2.0/PosturePerfection_0.2.0_armv7l"
-        chmod u+x PosturePerfection_0.2.0_armv7l 
+        wget "https://github.com/ESE-Peasy/PosturePerfection/releases/download/v0.3.0/PosturePerfection_0.3.0_armv7l"
+        chmod u+x PosturePerfection_0.3.0_armv7l 
         ;;
     *"x86"*)
         wget "https://github.com/ESE-Peasy/opencv/releases/download/v4.5.1-pp/opencv-4.5.1_x86.deb"
         sudo apt install ./opencv-4.5.1_x86.deb
-        wget "https://github.com/ESE-Peasy/PosturePerfection/releases/download/v0.2.0/PosturePerfection_0.2.0_x86_64"
-        chmod u+x PosturePerfection_0.2.0_x86_64
+        wget "https://github.com/ESE-Peasy/PosturePerfection/releases/download/v0.3.0/PosturePerfection_0.3.0_x86_64"
+        chmod u+x PosturePerfection_0.3.0_x86_64
         ;;
     *)
         printf -e "${RED}Unfortunately PosturePerfection is not yet supported on your system.${NO_COLOUR}\n"
@@ -39,4 +39,5 @@ sudo ldconfig
 
 printf "${B_GREEN}PosturePerfection has been successfully installed!\n\n"
 
-printf "${GREEN}Run ./PosturePerfection_0.2.0_xxx to run our graphical user interface which displays real time posture estimation. You will see the values for the co-ordinate and trustworthiness change as you move around your camera!${NO_COLOUR}\n\n"
+printf "${GREEN}Run ./PosturePerfection_0.3.0_xxx to run our graphical user interface which displays real time posture estimation. Lines are drawn between
+your Head, Neck, Shoulder and Hip to demonstrate your detected posture!${NO_COLOUR}\n\n"
