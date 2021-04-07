44b36f6ecd3ceeb5c285c190205cb07b066946ed
Ashwin <ashwinmj@gmail.com>
2021-04-07 16:36:36 +0100

    Ensure correct colour is displayed

--- src/posture_estimator.cpp
+++ src/posture_estimator.cpp
@@ -208,7 +208,7 @@ void PostureEstimator::display_current_pose(
           static_cast<int>(current_pose.joints.at(i).coord.y * imageHeight));
 
       cv::line(current_frame, upper_joint_point, current_joint_point,
-               posture_state == Good ? colours.at(Green) : colours.at(Blue), 5);
+               posture_state == Unset ? colours.at(Blue) : colours.at(Green), 5);
     }
   }
 }
